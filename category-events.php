a1351d80c1c92633cf9d88707911369c8307a32d
Matt Cascardi <mattcascardi@gmail.com>
2020-03-18 20:28:09 -0700

    Added events archive and search template

---
+++ category-events.php
@@ -0,0 +1,47 @@
+<?php
+
+get_header();
+
+if ( have_posts() ) { ?>
+
+
+    <div class="wrap">
+
+	<div id="primary" class="content-area">
+	    <main id="main" class="site-main" role="main">
+		<?php
+		/* Start the Loop */
+		while ( have_posts() ) {
+		    the_post();
+
+		    /*
+		     * Include the Post-Format-specific template for the content.
+		     * If you want to override this in a child theme, then include a file
+		     * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+		     */
+		    get_template_part( 'template-parts/post/content-excerpt');
+
+		}
+		the_posts_pagination(
+		    array(
+			'prev_text'          => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
+			'next_text'          => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
+			'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
+		    )
+		);
+
+		?>
+
+	    </main><!-- #main -->
+	</div><!-- #primary -->
+	<?php get_sidebar(); ?>
+    </div><!-- .wrap -->
+
+<?php
+} else {
+    get_template_part( '404' );
+}
+
+get_footer();
+
+//EOF
